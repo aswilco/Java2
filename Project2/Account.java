c746d970613817bf4d2a6ef1ebdf29c4d9b7314d
Aaron Wilcoxon <awilcoxon1@student.umuc.edu>
2017-02-03 12:36:21 -0500

    Update Account.java

--- Project2/Account.java
+++ Project2/Account.java
@@ -1,55 +1,70 @@
 import java.math.*;
 
 public class Account extends ATMMachine {
-	public static double checkingAccount;
-	public static double savingsBalance;
-	public static double savingsAccount;
-	public static double checkingBalance;
-	public static double accountBalance;
-	public static double currentCheckingBalance;
-	public static double currentSavingsBalance;
+	protected static double savingsBalance;
+	protected static double checkingBalance;
+	protected static double accountBalance;
 	private static int transactions = 0;
 	private static double transactionFee = 1.50;
 
+//Withdraw Method
 public static void Withdraw() throws InsufficientFunds {
-			try {
+		//Try to ParseInt from input to verify numerical value
+		try {
 			Integer.parseInt(ATMMachine.input.getText());
-	if (Integer.parseInt(ATMMachine.input.getText()) % 20 == 0) {
-	double amount = Integer.parseInt(ATMMachine.input.getText());
-		++transactions;
-	if (transactions > 4) {
-		amount = amount + transactionFee;
-	}
-	if (ATMMachine.checking.isSelected()) {
-	checkingBalance = checkingBalance - amount;
-		if (checkingBalance < 0) {
-			checkingBalance = checkingBalance + amount;
-			--transactions;
-			throw new InsufficientFunds();
-		}
-}
-	else if (ATMMachine.savings.isSelected()) {
-			savingsBalance = savingsBalance - amount;
-			if(savingsBalance < 0) {
-				savingsBalance = savingsBalance + amount;
-				--transactions;
-				throw new InsufficientFunds();
-			}
-}
+			//check if input is values of 20
+			if (Integer.parseInt(ATMMachine.input.getText()) % 20 == 0) {
+				double amount = Integer.parseInt(ATMMachine.input.getText());
+				++transactions;
+				if (transactions > 4) {
+					amount = amount + transactionFee;
+				}
+				//check if checking is selected and perform math on checkingBalance
+				if (ATMMachine.checking.isSelected()) {
+					checkingBalance = checkingBalance - amount;
+					//if not enough money throw insufficientFundsException
+					if (checkingBalance < 0) {
+						checkingBalance = checkingBalance + amount;
+						--transactions;
+						throw new InsufficientFunds();
+					}
+					ATMMachine.popUp.showMessageDialog(frame,
+					"New checking account balance: " + Account.checkingBalance);
+				}
+				//check if savings is selected and perform math on savingsBalance
+				else if (ATMMachine.savings.isSelected()) {
+					savingsBalance = savingsBalance - amount;
+					//if not enough money throw insufficientfunds exception
+					if(savingsBalance < 0) {
+						savingsBalance = savingsBalance + amount;
+						--transactions;
+						throw new InsufficientFunds();
+					}
+					ATMMachine.popUp.showMessageDialog(frame,
+					"New savings account balance: " + Account.savingsbalance);
+				}
+				else {
+					ATMMachine.popUp.showMessageDialog(ATMMachine.frame,
+					"Please select the desired account");
 
-}
-	else {ATMMachine.popUp.showMessageDialog(ATMMachine.frame,
-		"Amount must be in values of $20");
-}
-	}
-catch (NumberFormatException e) {
+			 }
+			//if input is not value of 20 give messageprompt
+			 else {
+				 ATMMachine.popUp.showMessageDialog(ATMMachine.frame,
+				"Amount must be in values of $20");
+			}
+		}
+		//catch parseInt exception and display input error
+		catch (NumberFormatException e) {
 		ATMMachine.popUp.showMessageDialog(ATMMachine.frame,
 		"Please input a number in values of $20");
-	}
+		}
 
 }
-
+	
+	//Deposit method for deposit button
 	public static void Deposit() {
+		//try to parse Int 
 		try {
 			Integer.parseInt(ATMMachine.input.getText());
 			double amount = Integer.parseInt(ATMMachine.input.getText());
@@ -59,8 +74,11 @@ catch (NumberFormatException e) {
 			} else if (ATMMachine.savings.isSelected()) {
 				savingsBalance = savingsBalance + amount;
 			}
-		} catch (NumberFormatException e) {
-			ATMMachine.popUp.showMessageDialog(ATMMachine.frame, "Input value must be numeric");
+		} 
+		//catch nfe for parse Int and display error
+		catch (NumberFormatException e) {
+			ATMMachine.popUp.showMessageDialog(ATMMachine.frame, 
+			"Please enter a number");
 		}
 	}
 
