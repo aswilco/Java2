3740a91537152368f6fd8c5e1c41e5396e9aeb19
Aaron Wilcoxon <awilcoxon1@student.umuc.edu>
2017-02-13 12:36:18 -0500

    Update Compute.java

--- Project3/Compute.java
+++ Project3/Compute.java
@@ -1,3 +1,34 @@
 public class Compute extends JFrame {
-
+  
+  static JFrame frame;
+  
+  private JPanel mainPanel;
+  
+  private JTextField n;
+  private JTextField result;
+  private JTextField efficiency;
+  
+  private JButton compute;
+  
+  private JRadioButton iterative;
+  private JRadioButton recursive;
+  private ButtonGroup method;
+  
+  
+  public Compute() {
+    super("Project 3");
+    setFrame(300, 500);
+    iterative = new JRadioButton("Iterative Method");
+    recursive = new JRadioButton("Recursive Method");
+    method.add(iterative);
+    method.add(recursive);
+      compute = new JButton( new AbstractAction("Compute") {
+       public void actionPerformed(ActionEvent e) {
+         n = Integer.parseInt(n.getText());
+         if (iterative.isSelected()) {
+          result.setText(computeIterative(n)); 
+         }
+         else if (recursive.isSelected()) {
+           result.setText(computeRecursive(n));
+         }
 }
