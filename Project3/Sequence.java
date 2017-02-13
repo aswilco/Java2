33176f3545c3145b9efb184d433580c58ff19458
Aaron Wilcoxon <awilcoxon1@student.umuc.edu>
2017-02-13 12:50:39 -0500

    Update Sequence.java

--- Project3/Sequence.java
+++ Project3/Sequence.java
@@ -2,11 +2,21 @@ Public class Sequence {
   
   private int n = Gui.getText(n);
   
-  public static int computeIterative() {
+  public static int computeIterative(int n) {
+    if (n == 1) {
+      results = 1
+    }
+    else if (n == 0) {
+      results = 0
+    }
+    else {
     int[] results = new int[n];
     for (i=1, i < n, i++) {
-      results[i] = (2(results[i-1]) + results[1-2]);
+      results[0] = 0;
+      results[1] = 1;
+      results[i] = (2(results[i-1]) + results[i-2]);
       return results[n];   
+    }
 }
     
     
