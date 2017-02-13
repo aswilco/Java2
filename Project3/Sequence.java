25cd6895b053be8880ae6f889a1f44c562e2fc53
Aaron Wilcoxon <awilcoxon1@student.umuc.edu>
2017-02-13 14:00:40 -0500

    Update Sequence.java

--- Project3/Sequence.java
+++ Project3/Sequence.java
@@ -10,32 +10,31 @@ Public class Sequence {
       results = 0
     }
     else {
-    int[] results = new int[n];
-    for (i=1, i < n, i++) {
-      results[0] = 0;
-      results[1] = 1;
-      results[i] = (2(results[i-1]) + results[i-2]);
-      return results[n];   
+      int[] results = new int[n];
+      for (i=1, i < n, i++) {
+        results[0] = 0;
+        results[1] = 1;
+        results[i] = (2(results[i-1]) + results[i-2]);
+        return results[n];   
+      }
     }
+  }
       
    public static int computeRecursive(int n) {
-     int i = 1;
-     n = this.n;
-     if (n == 1) {
-      results = 1
-    }
-    else if (n == 0) {
-      results = 0
-    }
-     else {
-       int[] results = new int[n];
-       if (i < n) {
-         results[i] = (2(results[i-1]) + results[i-2]);
-         i++
-           computeRecursive()
-       }
-     }
-         
+    if ( n >= 0) {
+      int i = 1;
+      n = this.n;
+      if (n == 0) {
+        return 0;
+      }
+      if (n == 1) {
+        return 1;
+      }
+      else {
+        return 2(computeRecursive(n-1)) + computeRecursive(n-2);
+      }
+    }        
+  }    
 }
     
     
