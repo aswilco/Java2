fdbac627e95b85604d0a253a079e6ceecb0f6dc2
Aaron Wilcoxon <awilcoxon1@student.umuc.edu>
2017-02-14 20:59:27 -0500

    Add files via upload

--- Project3/Compute.java
+++ Project3/Compute.java
@@ -1,57 +1,87 @@
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import java.text.*;
+
 public class Compute extends JFrame {
-  
+
   static JFrame frame;
-  
+
   private JPanel mainPanel;
-  
+
   private JTextField n;
   private JTextField result;
   private JTextField efficiency;
-  
+
   private JButton compute;
-  
+
   private JRadioButton iterative;
   private JRadioButton recursive;
   private ButtonGroup method;
   
-  public setVisible() {
-    setVisible(true);
-  }
-  
+  private String answer;
+
+	public void setVisible() {
+		setVisible(true);
+	}
+private void setFrame(int width, int height) {
+        setSize(width, height);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+
   public Compute() {
     super("Project 3");
     setFrame(300, 500);
-    
+
+	mainPanel = new JPanel();
+
     n = new JTextField("Enter an Integer:n");
-    
+
     result = new JTextField("Result is:");
     result.setEditable(false);
-    result.setBackGround(Color.LightGray);
+    result.setBackground(Color.lightGray);
     efficiency = new JTextField("Efficiency of Operation:");
     efficiency.setEditable(false);
-    efficiency.setBackGround(Color.LightGray);
-    
+    efficiency.setBackground(Color.lightGray);
+
     iterative = new JRadioButton("Iterative Method");
     recursive = new JRadioButton("Recursive Method");
-    
+    method = new ButtonGroup();
+
     method.add(iterative);
     method.add(recursive);
-    
+
       compute = new JButton( new AbstractAction("Compute") {
        public void actionPerformed(ActionEvent e) {
-         n = Integer.parseInt(n.getText());
          if (iterative.isSelected()) {
-          result.setText(computeIterative(n)); 
+			 answer = String.valueOf(Sequence.computeIterative(Integer.parseInt(n.getText())));
+			 result.setText(answer);
          }
          else if (recursive.isSelected()) {
-           result.setText(computeRecursive(n));
+			 Sequence.computeRecursive(Integer.parseInt(n.getText()));
+			 answer = String.valueOf(Sequence.computeRecursive(Integer.parseInt(n.getText())));
+			 result.setText(answer);
+
          }
-    
+	 }
+ });
+
     mainPanel.add(iterative);
     mainPanel.add(recursive);
     mainPanel.add(n);
     mainPanel.add(compute);
     mainPanel.add(result);
     mainPanel.add(efficiency);
-    
+    add(mainPanel);
 }
+public static void main(String[] args) {
+
+        Compute computer = new Compute();
+        computer.setVisible();
+        computer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
+
+
--- Project3/Sequence.java
+++ Project3/Sequence.java
@@ -1,42 +1,34 @@
-Public class Sequence {
-  
-  
+public class Sequence {
+
+private static int results;
+private static int i;
+private static int result;
+private static int runningResult;
+private static int callsToRecursive;
+
   public static int computeIterative(int n) {
-    if (n == 1) {
-      results = 1
-    }
-    else if (n == 0) {
-      results = 0
-    }
-    else {
-      int[] results = new int[n];
-      for (i=1, i < n, i++) {
-        results[0] = 0;
-        results[1] = 1;
-        results[i] = (2(results[i-1]) + results[i-2]);
-        return results[n];   
-      }
-    }
-  }
-      
+}
+
    public static int computeRecursive(int n) {
-     int callsToRecursive;
+ 
      callsToRecursive++;
     if ( n >= 0) {
       int i = 1;
-      n = this.n;
       if (n == 0) {
         return 0;
       }
-      if (n == 1) {
+      else if (n == 1) {
         return 1;
       }
-      else {
-        return 2(computeRecursive(n-1)) + computeRecursive(n-2);
-      }
-    } 
-     
-   public static int getEfficiency() {
+
+          else {
+        result = 2 * computeRecursive(n-1) + computeRecursive(n-2);
+	}
+}
+return result;
+}
+
+   /*public static int getEfficiency() {
      if (Compute.iterative.isSelected()) {
        return i;
      }
@@ -44,8 +36,8 @@ Public class Sequence {
        return callsToRecursive;
      }
    }
-         
-  }    
-}
-    
-    
+*/
+  }
+
+
+
