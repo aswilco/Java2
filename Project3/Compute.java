4f0c8c2d2a96d63dbb2724af759f5490e1aca154
aswilco <awilcoxon1@student.umuc.edu>
2017-02-16 23:20:06 -0500

    eqfeq

--- Project3/Compute.java
+++ Project3/Compute.java
@@ -2,15 +2,23 @@ import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
 import java.text.*;
+import java.io.*;
 
 public class Compute extends JFrame {
 
   static JFrame frame;
 
-  private JPanel mainPanel;
+  private JPanel contentPane;
+  private JPanel methodPanel;
+  private JPanel inputPanel;
+  private JPanel buttonPanel;
+  private JPanel outputPanel;
 
+  private JLabel nLabel;
   private JTextField n;
+  private JLabel resultLabel;
   private JTextField result;
+  private JLabel efficiencyLabel;
   private JTextField efficiency;
 
   private JButton compute;
@@ -19,35 +27,64 @@ public class Compute extends JFrame {
   private JRadioButton recursive;
   private ButtonGroup method;
   
+  private BufferedWriter fileOutput;
+  
+  private int i;
+  
+  private WindowListener adapter;
+  
   private String answer;
 
 	public void setVisible() {
 		setVisible(true);
 	}
+	
 private void setFrame(int width, int height) {
         setSize(width, height);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 }
 
+
   public Compute() {
-    super("Project 3");
-    setFrame(300, 500);
-
-	mainPanel = new JPanel();
-
-    n = new JTextField("Enter an Integer:n");
-
-    result = new JTextField("Result is:");
+     
+    
+    frame = new JFrame("Iterative vs Recursive");
+    frame.setSize(300,300);
+	frame.setLocationRelativeTo(null);
+	contentPane = new JPanel();
+	methodPanel = new JPanel();
+	inputPanel = new JPanel();
+	buttonPanel = new JPanel();
+	outputPanel = new JPanel();
+	
+	nLabel = new JLabel("Enter an Integer:");
+    n = new JTextField();
+    n.setColumns(10);
+    inputPanel.add(nLabel);
+    inputPanel.add(n);
+    
+    resultLabel = new JLabel("Result is:");
+    result = new JTextField();
     result.setEditable(false);
     result.setBackground(Color.lightGray);
-    efficiency = new JTextField("Efficiency of Operation:");
+    result.setColumns(15);
+    efficiencyLabel = new JLabel("Efficiency of Operation:");
+    efficiency = new JTextField();
     efficiency.setEditable(false);
     efficiency.setBackground(Color.lightGray);
+    efficiency.setColumns(15);
+    outputPanel.add(resultLabel);
+    outputPanel.add(result);
+    outputPanel.add(efficiencyLabel);
+    outputPanel.add(efficiency);
+    outputPanel.setLayout(new GridLayout(0, 1));
 
     iterative = new JRadioButton("Iterative Method");
     recursive = new JRadioButton("Recursive Method");
     method = new ButtonGroup();
+    methodPanel.add(iterative);
+    methodPanel.add(recursive);
 
     method.add(iterative);
     method.add(recursive);
@@ -57,29 +94,73 @@ private void setFrame(int width, int height) {
          if (iterative.isSelected()) {
 			 answer = String.valueOf(Sequence.computeIterative(Integer.parseInt(n.getText())));
 			 result.setText(answer);
+			 efficiency.setText(String.valueOf(Sequence.GetEfficiency()));
+			 Sequence.efficiency = 0;
          }
          else if (recursive.isSelected()) {
 			 Sequence.computeRecursive(Integer.parseInt(n.getText()));
 			 answer = String.valueOf(Sequence.computeRecursive(Integer.parseInt(n.getText())));
 			 result.setText(answer);
+ 			 efficiency.setText(String.valueOf(Sequence.GetEfficiency()));
+ 			 Sequence.efficiency = 0;
+
 
          }
 	 }
- });
+		});
+		
+		buttonPanel.add(compute);
+		
+ contentPane.add(methodPanel);
+ contentPane.add(inputPanel);
+ contentPane.add(buttonPanel);
+ contentPane.add(outputPanel);
+frame.add(contentPane);
+ 
+	frame.getContentPane();
+	frame.setVisible(true);
 
-    mainPanel.add(iterative);
-    mainPanel.add(recursive);
-    mainPanel.add(n);
-    mainPanel.add(compute);
-    mainPanel.add(result);
-    mainPanel.add(efficiency);
-    add(mainPanel);
+	frame.addWindowListener(new WindowAdapter () {
+		public void windowClosing(WindowEvent e) {
+		System.out.println("Please Wait...Calculating Effiiency");
+		File file = new File("Efficiency.csv");
+		try {
+				fileOutput = new BufferedWriter(new FileWriter("Efficiency.csv"));
+			}
+			catch (IOException v) {
+				System.out.println("File Does Not Exist");
+			}
+		
+		for (i=0; i <= 10; i++){
+			try {
+			Sequence.computeIterative(i);
+			fileOutput.write(String.valueOf(i));
+			fileOutput.write( "," );
+			fileOutput.write(String.valueOf(Sequence.GetEfficiency()));
+			fileOutput.write( ",");
+			Sequence.computeRecursive(i);
+			fileOutput.write(String.valueOf(Sequence.GetEfficiency()));
+			fileOutput.newLine();
+			fileOutput.flush();
+
+			}
+		catch (IOException c) {
+			System.out.println("File Does Not Exist");
+		}
+		}
+		System.out.println("Closing");
+		System.exit(0);		
+	}
+});
+	
+	
+   
 }
 public static void main(String[] args) {
 
         Compute computer = new Compute();
         computer.setVisible();
-        computer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //computer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
--- Project3/Sequence.java
+++ Project3/Sequence.java
@@ -1,52 +1,51 @@
-import java.util.ArrayList;
 
 public class Sequence {
 
-private static int results;
 private static int i;
-private static int result;
-private static int runningResult;
+private static long result;
+private static long runningResult;
 private static int callsToRecursive;
-private ArrayList<Integer> results = new ArrayList<Integer>();
+protected static int efficiency;
+private static int totalEfficiency;
 
 
-  public static int computeIterative(int n) {
-	  for (i = 0; i < n; i++) {
-		  if (i == 0) {
-			  results.add(0)
-		  }
-		  if (i == 1) {
-			  results.add(1)
-		  }
-		  else {
-			  results.add(2 * results.get(results.size()-1) + results.get(results.size()-2));
-		  }
-		  result = results.get(n);
+  public static long computeIterative(int n) {
+	  long secPrevious = 0, previous = 1, current = 0;
+	  if (n == 0) {
+		  current = 0;
 	  }
-	  
+	  else if (n == 1) {
+		  current = 1;
+	  }
+	  else {
+	  for (i = 0; i < n - 1; i++) {
+		  GetEfficiency();
+		  current = 2 * previous + secPrevious;
+		  secPrevious = previous;
+		  previous = current;
+	  }
+	  }
+	  return current;
 }
 
-   public static int computeRecursive(int n) {
- 
-     callsToRecursive++;
-    if ( n >= 0) {
-      int i = 1;
+   public static long computeRecursive(int n) {
+	GetEfficiency();
       if (n == 0) {
-        return 0;
+        result = 0;
       }
       else if (n == 1) {
-        return 1;
+        result = 1;
       }
-
-          else {
+      else if (n >= 2) {
         result = 2 * computeRecursive(n-1) + computeRecursive(n-2);
-	}
-}
+	  }
 return result;
 }
 
-   public static int getEfficiency() {
+   public static int GetEfficiency() {
 	efficiency++;
+	totalEfficiency = efficiency;
+	   return totalEfficiency;
    }
   }
 
